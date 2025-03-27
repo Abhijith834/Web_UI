9aa7999ab21f9c108b6be5e1bdb90e4b13f4d866
Abhijith834 <abhijithsaji834@gmail.com>
2025-03-27 12:59:06 +0000

    chat switching enabled

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
+// App.jsx
 import React from "react";
+import { Routes, Route } from "react-router-dom";
 import ChatLayout from "./components/ChatLayout";
+import MCQ from "./components/MCQ"; // Import MCQ
 import "./index.css";
 
 function App() {
   return (
     <div className="w-full min-h-screen flex flex-col text-white">
-      <ChatLayout />
+      <Routes>
+        <Route path="/" element={<ChatLayout />} />
+        <Route path="/mcq" element={<MCQ />} />
+      </Routes>
     </div>
   );
 }
