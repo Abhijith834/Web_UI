ce0c17a2af0ee4fcba042700f5f8c12ba74ed0f0
Abhijith834 <abhijithsaji834@gmail.com>
2025-04-10 13:45:21 +0100

    vite config

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/Web_UI/',
   plugins: [
     tailwindcss(),
     react()],
